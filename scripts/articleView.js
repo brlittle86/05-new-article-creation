0cb1d0669156516503fa8dd81b0857922b43b8e3
Brandon Little <brlittle86@gmail.com>
2017-02-10 16:14:36 -0800

    completed Handlebars interface TODO

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -106,8 +106,8 @@ articleView.create = function() {
   });
 
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  $('#articles').append(article.toHtml());
 
   // TODO: Activate the highlighting of any code blocks:
   $('pre code').each(function(i, block) {
